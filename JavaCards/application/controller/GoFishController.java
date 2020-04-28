7e0a96aea00c49598e2d158c432d301e74119dce
dyches <61217427+dyches@users.noreply.github.com>
2020-04-27 20:57:48 -0500

    Add files via upload

--- JavaCards/application/controller/GoFishController.java
+++ JavaCards/application/controller/GoFishController.java
@@ -3,9 +3,15 @@
  */
 package application.controller;
 
+import java.util.ArrayList;
+
 import application.controller.JavaCardsController.ScreenModes;
+import application.model.BlackJackGame;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * @author Jennifer Ingram UTSA ID drd411
@@ -13,8 +19,22 @@ import javafx.fxml.FXML;
  *
  */
 public class GoFishController {
-	private String userName;
-	private ScreenModes screenMode = ScreenModes.DEFAULT;
+	private String userName; // Holds user name from main screen (JavaCards)
+	private ScreenModes screenMode = ScreenModes.DEFAULT; // Holds screen mode from main screen (JavaCards)
+	private GoFishGame goFishGame; // Data model for Go Fish Game
+	ArrayList<ImageView> newUserImageViewList; // Holds dynamically created card ImageViews for User Deck
+	ArrayList<ImageView> newDealerImageViewList; // Holds dynamically created card ImageViews for Dealer Deck
+	
+    @FXML
+    private AnchorPane goFishAnchorPane; // Main Pane
+    
+    @FXML
+    private Button goFishNewGameBtn; // Button to clear screen and start new game
+    
+    @FXML
+    private Button callGameBtn; // Button to end game on user click
+    
+    // TODO: add user hand as buttons
 
     @FXML
     public void newGameBtnClicked(ActionEvent event) {
