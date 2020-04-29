fe68c51bb6c382bdb0f25fc33944940594492ba5
dyches <61217427+dyches@users.noreply.github.com>
2020-04-29 16:26:43 -0500

    Add files via upload
    started implementation of screen modes

--- JavaCards/application/controller/JavaCardsController.java
+++ JavaCards/application/controller/JavaCardsController.java
@@ -11,6 +11,8 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
 
 
 /**
@@ -33,7 +35,12 @@ public class JavaCardsController {
 
     @FXML
     private RadioButton rb_dark;
-
+/*  
+    @FXML
+    private ToggleGroup tg_screenmodes = new ToggleGroup();
+    this.rb_default.setToggleGroup(tg_screenmodes);
+    this.rb_dark.setToggleGroup(tg_screenmodes);
+*/
 	@FXML
     private Button playBlackJackBtn;
 
@@ -41,15 +48,47 @@ public class JavaCardsController {
     private Button playGoFishBtn;
 
     @FXML
+    /**
+     * reloads the main screen in default mode and sets screenMode to DEFAULT
+     * @param event
+     */
     void rb_defaultClicked(ActionEvent event) {
     	screenMode = ScreenModes.DEFAULT;
     	// TODO: Write code to change screen colors and refresh screen
+    	try {
+			Stage newStage = new Stage();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("view/JavaCards.fxml"));
+			AnchorPane root = (AnchorPane)loader.load();
+			Scene scene = new Scene(root,547,410);
+			scene.getStylesheets().add(getClass().getResource("..//view/application.css").toExternalForm());
+			newStage.setScene(scene);
+			newStage.setTitle("Java Cards");
+			newStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
     }
     
     @FXML
+    /**
+     * reloads the main screen in dark mode and sets screenMode to DARKMODE
+     * @param event
+     */
     void rb_darkClicked(ActionEvent event) {
     	screenMode = ScreenModes.DARKMODE;
     	// TODO: Write code to change screen colors and refresh screen
+    	try {
+			Stage newStage = new Stage();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("view/JavaCards.fxml"));
+			AnchorPane root = (AnchorPane)loader.load();
+			Scene scene = new Scene(root,547,410);
+			scene.getStylesheets().add(getClass().getResource("..//view/darkmode.css").toExternalForm());
+			newStage.setScene(scene);
+			newStage.setTitle("Java Cards");
+			newStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
     }
     
     @FXML
@@ -77,7 +116,12 @@ public class JavaCardsController {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("..//view//BlackJack.fxml"));
 			Parent root = loader.load();
 			Scene scene = new Scene(root,600,600);
-			scene.getStylesheets().add(getClass().getResource("..//view/application.css").toExternalForm());
+			if(screenMode == ScreenModes.DEFAULT) {
+				scene.getStylesheets().add(getClass().getResource("..//view/application.css").toExternalForm());
+			}
+			else if(screenMode == ScreenModes.DARKMODE) {
+				scene.getStylesheets().add(getClass().getResource("..//view/darkmode.css").toExternalForm());
+			}
 			blackJackStage.initModality(Modality.APPLICATION_MODAL);
 			blackJackStage.setScene(scene);
 			blackJackStage.setTitle("Black Jack");
@@ -91,27 +135,32 @@ public class JavaCardsController {
 		}
 	}
 	
-	 /**
-		 * Launch BlackJackController
-		 * 
-		 */
-		private void LaunchGoFishScreen() {
-			try {
-				Stage goFishStage = new Stage();
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("..//view//GoFish.fxml"));
-				Parent root = loader.load();
-				Scene scene = new Scene(root,600,600);
-				scene.getStylesheets().add(getClass().getResource("..//view/application.css").toExternalForm());
-				goFishStage.initModality(Modality.APPLICATION_MODAL);
-				goFishStage.setScene(scene);
-				goFishStage.setTitle("Go Fish");
-				
-				GoFishController controller = loader.<GoFishController>getController();
-				controller.initData(userName, screenMode);
-				
-				goFishStage.show();
-			} catch(Exception e) {
-				e.printStackTrace();
+	/**
+	* Launch BlackJackController
+	* 
+	*/
+	private void LaunchGoFishScreen() {
+		try {
+			Stage goFishStage = new Stage();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("..//view//GoFish.fxml"));
+			Parent root = loader.load();
+			Scene scene = new Scene(root,600,600);
+			if(screenMode == ScreenModes.DEFAULT) {
+				scene.getStylesheets().add(getClass().getResource("..//view/application.css").toExternalForm());			
 			}
+			else if(screenMode == ScreenModes.DARKMODE) {
+				scene.getStylesheets().add(getClass().getResource("..//view/darkmode.css").toExternalForm());
+			}
+			goFishStage.initModality(Modality.APPLICATION_MODAL);
+			goFishStage.setScene(scene);
+			goFishStage.setTitle("Go Fish");
+
+			GoFishController controller = loader.<GoFishController>getController();
+			controller.initData(userName, screenMode);
+				
+			goFishStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
+	}
 }
